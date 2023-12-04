6fef8bcec7b37d2e2e3b649d9258914e09e37c40
ikin <ikin@hifor.net>
2023-12-04 15:17:32 +0800

    rename

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ javaFX demo
 
 - 生成exe
 
-  执行以下命令即可自动生成exe文件目录 ./build/jpackage/hi-javaFX
+  执行以下命令即可自动生成exe文件目录 ./build/jpackage/hi-javafx
 ```shell
 ./gradlew jpackage
 ```
--- src/main/java/net/hifor/demo/HelloController.java
+++ src/main/java/net/hifor/demo/HelloController.java
@@ -3,6 +3,9 @@ package net.hifor.demo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * @author IKin
+ */
 public class HelloController {
     @FXML
     private Label welcomeText;
